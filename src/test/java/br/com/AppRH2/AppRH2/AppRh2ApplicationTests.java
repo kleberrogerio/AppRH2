ae143d821497b3b425a52a9d17dacd0a2912971c
kleberrogerio <kleber.nascimento5@fatec.sp.gov.br>
2024-09-11 12:03:10 -0300

    Inicial

---
+++ src/test/java/br/com/AppRH2/AppRH2/AppRh2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.AppRH2.AppRH2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRh2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
