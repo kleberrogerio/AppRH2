899a0f3bf9d69b9597ca10ca15c8d06e12804986
kleberroger <kleber.nascimento5@fatec.sp.gov.br>
2024-09-10 21:40:42 -0300

    Inicial

---
+++ src/main/java/br/com/AppRH2/AppRH2/DataConfiguration.java
@@ -0,0 +1,44 @@
+package br.com.AppRH2.AppRH2;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+
+@Configuration
+public class DataConfiguration {
+	
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/serco?useTimezone=true&serverTimezone=America/Sao_Paulo");
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+		return dataSource;
+	}
+	
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MariaDBDialect");
+		adapter.setPrepareConnection(true);
+		return adapter;
+	}
+	
+	@Bean
+	public Connection connection(DataSource dataSource) throws SQLException {
+		return dataSource.getConnection();
+	}
+
+}
