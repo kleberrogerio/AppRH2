820796c75fda9301df3d575a82c6fef7e5404066
kleberrogerio <kleber.nascimento5@fatec.sp.gov.br>
2024-09-12 13:07:58 -0300

    Iniciando os Models

---
+++ src/main/java/br/com/AppRH2/AppRH2/models/Vaga.java
@@ -0,0 +1,15 @@
+package br.com.AppRH2.AppRH2.models;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+
+public class Vaga {
+
+}
