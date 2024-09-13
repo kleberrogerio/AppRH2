10568079c701fa741f917c8976923775d21ac803
kleberrogerio <kleber.nascimento5@fatec.sp.gov.br>
2024-09-13 13:08:00 -0300

    Iniciando o controller

--- src/main/java/br/com/AppRH2/AppRH2/models/Candidato.java
+++ src/main/java/br/com/AppRH2/AppRH2/models/Candidato.java
@@ -1,5 +1,69 @@
 package br.com.AppRH2.AppRH2.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+
+@Entity
 public class Candidato {
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	@Column(unique=true)
+	private String rg;
+	
+	@NotNull
+	private String nomeCandidato;
+	
+	@NotNull
+	private String email;
+	
+	@ManyToOne
+	private Vaga vaga;
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getNomeCandidato() {
+		return nomeCandidato;
+	}
+
+	public void setNomeCandidato(String nomeCandidato) {
+		this.nomeCandidato = nomeCandidato;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Vaga getVaga() {
+		return vaga;
+	}
+
+	public void setVaga(Vaga vaga) {
+		this.vaga = vaga;
+	}
+	
+	
+	
+	
+	
+	
+	
 
 }
--- src/main/java/br/com/AppRH2/AppRH2/models/Vaga.java
+++ src/main/java/br/com/AppRH2/AppRH2/models/Vaga.java
@@ -4,14 +4,14 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 
-import javax.persistence.OneToMany;
-
-
+@Entity
 public class Vaga implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
