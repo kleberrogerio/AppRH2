66227921a7ab9ae24bd05e2c5d161fed88d7ecae
kleberroger <kleber.nascimento5@fatec.sp.gov.br>
2024-09-15 22:14:54 -0300

    Iniciando os controllers

--- src/main/java/br/com/AppRH2/AppRH2/controllers/VagaController.java
+++ src/main/java/br/com/AppRH2/AppRH2/controllers/VagaController.java
@@ -1,5 +1,18 @@
 package br.com.AppRH2.AppRH2.controllers;
 
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+
+
+
 public class VagaController {
 
 }
